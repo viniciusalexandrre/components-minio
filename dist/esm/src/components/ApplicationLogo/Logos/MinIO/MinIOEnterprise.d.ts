99e4196f70fc66203a2857a034abaf725dec9d25
Vinicius <vinicius.valle@altatech.com.br>
2025-08-20 09:56:54 -0300

    novo dist

--- dist/esm/src/components/ApplicationLogo/Logos/MinIO/MinIOEnterprise.d.ts
+++ dist/esm/src/components/ApplicationLogo/Logos/MinIO/MinIOEnterprise.d.ts
@@ -1,4 +1,4 @@
-import { FC, SVGProps } from "react";
-import { LogoBaseProps } from "../LogoBase/LogoBase.types";
+import { FC, SVGProps } from 'react';
+import { LogoBaseProps } from '../LogoBase/LogoBase.types';
 declare const MinIOEnterprise: FC<SVGProps<any> & LogoBaseProps>;
 export default MinIOEnterprise;
--- dist/esm/src/components/ApplicationLogo/Logos/MinIO/MinIOEnterpriseOSVertical.d.ts
+++ dist/esm/src/components/ApplicationLogo/Logos/MinIO/MinIOEnterpriseOSVertical.d.ts
@@ -1,4 +1,4 @@
-import { FC, SVGProps } from "react";
-import { LogoBaseProps } from "../LogoBase/LogoBase.types";
+import { FC, SVGProps } from 'react';
+import { LogoBaseProps } from '../LogoBase/LogoBase.types';
 declare const MinIOEnterpriseOSVertical: FC<SVGProps<any> & LogoBaseProps>;
 export default MinIOEnterpriseOSVertical;
